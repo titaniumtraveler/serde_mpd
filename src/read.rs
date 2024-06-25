5d170af07690f5e4e601c3377e6284891d919388
Jon Heinritz <jon.heinritz@protonmail.com>
2024-06-25 06:05:05 +0200

    doc: add basic implementors notes to Read trait methods

--- src/read.rs
+++ src/read.rs
@@ -5,11 +5,15 @@ pub trait Read<'de> {
     fn next(&mut self) -> Result<Option<u8>>;
     fn discard(&mut self);
 
+    /// Reads in bytes until the `until` byte is encountered and then returns a [`Reference`] to it.
+    /// After usage of the [`Reference`] the scratch buffer has to be [`Vec::clear()`]ed.
     fn read_until<'s>(
         &mut self,
         scratch: &'s mut Vec<u8>,
         until: u8,
     ) -> Result<Reference<'de, 's, [u8]>>;
+    /// Reads in len bytes and then returns a [`Reference`] to it.
+    /// After usage of the [`Reference`] the scratch buffer has to be [`Vec::clear()`]ed.
     fn read_len<'s>(
         &mut self,
         scratch: &'s mut Vec<u8>,
@@ -18,7 +22,10 @@ pub trait Read<'de> {
 
     fn skip_until(&mut self, until: u8) -> Result<()>;
 
-    fn starts_with(&mut self, scratch: &mut Vec<u8>, starts_with: &str) -> Result<bool>;
+    /// Checks if input starts with `starts_with` and discards those bytes if yes.
+    /// Might use scratch to buffer the bytes in scratch and will [`Vec::clear()`] it itself in
+    /// case of a positive result. (`Ok(true)`)
+    fn starts_with(&mut self, scratch: &mut Vec<u8>, starts_with: &[u8]) -> Result<bool>;
 }
 
 pub enum Reference<'de, 's, T>
