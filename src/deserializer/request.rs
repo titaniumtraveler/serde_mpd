41f800cabcacf720c4e027ddb96e5159c602e005
Jon Heinritz <jon.heinritz@protonmail.com>
2024-06-26 13:57:21 +0200

    feat: add impl for RequestDeserializer::deserialize_ignored_any()

--- src/deserializer/request.rs
+++ src/deserializer/request.rs
@@ -5,6 +5,14 @@ use serde::{de::Visitor, forward_to_deserialize_any, Deserializer};
 pub struct RequestDeserializer<R> {
     read: R,
     scratch: Vec<u8>,
+    state: DeserializerState,
+}
+
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
+enum DeserializerState {
+    #[default]
+    None,
+    IgnoreAnyAck,
 }
 
 impl<'de, R: Read<'de>> Deserializer<'de> for &mut RequestDeserializer<R> {
@@ -36,10 +44,32 @@ impl<'de, R: Read<'de>> Deserializer<'de> for &mut RequestDeserializer<R> {
         todo!()
     }
 
-    fn deserialize_ignored_any<V>(self, _visitor: V) -> Result<V::Value, Self::Error>
+    fn deserialize_ignored_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
     where
         V: Visitor<'de>,
     {
-        todo!()
+        match self.state {
+            DeserializerState::None => {
+                if self.read.starts_with(&mut self.scratch, b"ACK ")? {
+                    self.state = DeserializerState::IgnoreAnyAck;
+                    self.read.skip_until(b'\n')?;
+                    self.state = DeserializerState::None;
+                    return visitor.visit_unit();
+                }
+
+                loop {
+                    if self.read.starts_with(&mut self.scratch, b"OK\n")? {
+                        break visitor.visit_unit();
+                    } else {
+                        self.read.skip_until(b'\n')?;
+                    }
+                }
+            }
+            DeserializerState::IgnoreAnyAck => {
+                self.read.skip_until(b'\n')?;
+                self.state = DeserializerState::None;
+                visitor.visit_unit()
+            }
+        }
     }
 }
