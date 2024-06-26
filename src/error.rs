f62e150059133baa8dc074a7c4308a5d13260629
Jon Heinritz <jon.heinritz@protonmail.com>
2024-06-26 11:24:20 +0200

    feat: impl PartialEq, Eq, PartialOrd, Ord for Error

--- src/error.rs
+++ src/error.rs
@@ -5,10 +5,10 @@ use std::{
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Error {
     Custom(String),
-    Io(io::Error),
+    Io(Io),
     /// Some IO operation is pending. If this error is thrown it is safe to retry the operation.
     Pending,
     Eof,
@@ -33,5 +33,36 @@ impl serde::de::Error for Error {
         T: Display,
     {
         Error::Custom(msg.to_string())
+    }
+}
+
+#[repr(transparent)]
+pub struct Io(pub io::Error);
+
+impl Debug for Io {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        Debug::fmt(&self.0, f)
+    }
+}
+impl Display for Io {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        Display::fmt(&self.0, f)
+    }
+}
+
+impl PartialEq for Io {
+    fn eq(&self, other: &Self) -> bool {
+        PartialEq::eq(&self.0.kind(), &other.0.kind())
+    }
+}
+impl Eq for Io where io::ErrorKind: Eq {}
+impl PartialOrd for Io {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl Ord for Io {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        Ord::cmp(&self.0.kind(), &other.0.kind())
     }
 }
